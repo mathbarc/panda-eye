f3b247772b737f9b3a791942fba24f42e2cadd47
Matheus Barcelos de Oliveira <matheus.oliveira@syngenta.com>
2023-02-18 16:16:56 -0300

    treating videos

--- Core/source/video.cpp
+++ Core/source/video.cpp
@@ -2,14 +2,19 @@
 #include "utils.hpp"
 #include <QImage>
 
-const QStringList Video::filters = QStringList() << "*.mpeg"<< "*.MPEG" << "*.mp4" << "*.MP4" << "*.avi" << "*.avi" <<"*.mkv"<<"*.MKV";
+const QStringList Video::filters = QStringList() << "*.mpeg"<< "*.MPEG" << "*.mp4" << "*.MP4" << "*.avi" << "*.avi" <<"*.mkv"<<"*.MKV"<<"*.mov"<<"*.MOV";
 
 Video::Video(QString filePath)
     : Media(filePath.toStdString())
 {
-    cv::VideoCapture video(this->path);
+    cv::VideoCapture video(this->path, cv::CAP_FFMPEG);
     cv::Mat frame;
     video >> frame;
+
+    if(frame.empty())
+    {
+        frame = cv::Mat::zeros(cv::Size(200,200),CV_8UC3);
+    }
 
     this->thumbnail = utils::cvMatToQImage(utils::resizeSquarred(frame, Media::thumbnailSize));
     video.release();
