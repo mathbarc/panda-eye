1b90cf0eb4c42de01716e5fa8566c8639a7fd6a1
Andre Emidio <andresjc2008@gmail.com>
2023-07-29 19:56:59 -0300

    chore : remoção do ponteiro

--- Core/source/image.cpp
+++ Core/source/image.cpp
@@ -1,21 +1,15 @@
 #include "image.hpp"
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgcodecs/imgcodecs.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
 #include "utils.hpp"
 
 
-#include "dcmtk/config/osconfig.h"
-#include "dcmtk/dcmdata/dcfilefo.h"
-#include "dcmtk/dcmdata/libi2d/i2d.h"
-#include "dcmtk/dcmdata/libi2d/i2djpgs.h"
 #include "dcmtk/dcmdata/libi2d/i2dplsc.h"
-#include "dcmtk/dcmdata/dctk.h"
 
 
 const QStringList Image::filters =
         QStringList() << "*.jpg" << "*.JPG" << "*.jpeg" << "*.JPEG" << "*.png" << "*.PNG" << "*.tiff" << "*.TIFF"
-                      << ".dcm" << ".DCM";
+                      << ".bmp" << ".BMP" << ".dcm" << ".DCM";
 
 Image::Image(QString path)
         : Media(path.toStdString()) {
@@ -37,13 +31,13 @@ Image::Image(QString path)
 
     else if (path.contains(".dcm") || path.contains(".DCM")) {
 
-        DicomImage *dcmImage = new DicomImage(this->path.c_str());
-        int depth = dcmImage->getDepth();
-        int width = dcmImage->getWidth();
-        int height = dcmImage->getHeight();
-        unsigned short *pixelData = (unsigned short *) (dcmImage->getOutputData(16));
+        DicomImage dcmImage (this->path.c_str());
+        int width = dcmImage.getWidth();
+        int height = dcmImage.getHeight();
+        unsigned short *pixelData = (unsigned short *) (dcmImage.getOutputData(16));
         cv::Mat imageMat(height, width, CV_16UC1, pixelData);
-        imageMat.copyTo(img);
+
+        img = imageMat;
     }
 
 #endif
